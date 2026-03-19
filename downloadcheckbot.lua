da58e4b3817191808f5d1fe07724794852e3d518
Bgzs89 <135304744+Bgzs89@users.noreply.github.com>
2026-03-19 11:30:58 +0700

    Update downloadcheckbot.lua

--- downloadcheckbot.lua
+++ downloadcheckbot.lua
@@ -1,7 +1,7 @@
 -- COPY THIS TEXT AND PASTE IN LUCIFER EXECUTOR
 
 link = "your webhook link"
-messageId = msg id -- if u dont have, use **messageId = nil** then copy id and put in here
+messageId = msg id -- if u dont have msg id or first time using this, use *messageId = nil* then copy the message id and put in here
 
 local client = HttpClient.new()
 client.url = "https://raw.githubusercontent.com/Bgzs89/statusbotonineoffline/refs/heads/main/updated%20checkbot%20v1.4.lua"
